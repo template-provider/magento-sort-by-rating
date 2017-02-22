e273ffea50c8970921c6a252b121467d64a8ac84
Jan Gantzert <jgantzert@template-provider.com>
2017-02-22 19:55:09 +0100

    Installer now with complete intital data

--- app/code/community/Tp/SortRating/sql/sortrating_setup/mysql4-install-1.0.0.php
+++ app/code/community/Tp/SortRating/sql/sortrating_setup/mysql4-install-1.0.0.php
@@ -3,12 +3,28 @@ $installer = $this;
 
 $installer->startSetup();
 
-$productEntityTable = $installer->getTable('catalog/product');
+$productTable = $installer->getTable('catalog/product');
+$reviewAggregate = $installer->getTable('review/review_aggregate');
 
 /** @var Mage_Core_Model_Resource_Store_Collection $stores */
 $stores = Mage::getResourceModel('core/store_collection');
 foreach ($stores as $store) {
+    $storeId = $store->getId();
+
     $ratingSummaryStore = 'sort_rating_' . strtolower($store->getCode());
-    $installer->getConnection()->addColumn($productEntityTable, $ratingSummaryStore, "INT DEFAULT NULL");
+    $installer->getConnection()->addColumn($productTable, $ratingSummaryStore, "INT DEFAULT NULL");
+
+    $subQuery = "select AVG(rating_summary) as sort_rating, store_id, entity_pk_value
+from {$reviewAggregate}
+where reviews_count > 0
+AND store_id = {$storeId}
+group by entity_pk_value, store_id";
+
+    $query = "Update {$productTable} as C
+inner join ($subQuery) as A on C.entity_id = A.entity_pk_value
+set C.{$ratingSummaryStore} = A.sort_rating";
+
+    $installer->getConnection()->query($query);
+
 }
 $installer->endSetup();
